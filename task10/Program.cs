b2a9acb56744d64a0342e93f36666b341c972d51
Sonya <vilinskaya.s@bk.ru>
2023-03-14 14:22:01 +0300

    Добавили решение 10, 13, 15 задач

---
+++ task10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<100 || number > 1000)
+{
+    Console.WriteLine("Ошибка, ведите трёхзначное число");
+    return;
+}
+
+int firstDigit = number % 100;
+int secondDigit = firstDigit / 10;
+Console.WriteLine(secondDigit);
---
+++ task13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number<100)
+{
+    Console.Write("Ошибка, в числе меньше 3 цифр. Введите новое число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+while (number > 999)
+{
+   number = number / 10;
+}
+number = number % 10;
+Console.WriteLine(number);
